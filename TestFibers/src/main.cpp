056713389310f378b7790ea5b64519607437aea1
James T. Matta <jamesmatta@gmail.com>
2018-06-13 17:57:26 -0400

    Updated to support user set fibers and iterations
    Signed-Off-By: James Till Matta <james.matta@cadre5.com>

--- TestFibers/src/main.cpp
+++ TestFibers/src/main.cpp
@@ -1,141 +1,83 @@
 #include<time.h>
 #include<iostream>
 #include<string>
+#include<vector>
+#include<sstream>
 #include<boost/bind.hpp>
 #include<boost/fiber/all.hpp>
 
-typedef boost::fibers::buffered_channel< std::string > fifo_t;
+typedef boost::fibers::buffered_channel< std::string > FifoType;
+typedef boost::fibers::fiber FiberType;
 
-static const int iterCount = 1000000;
 static const clockid_t clockType = CLOCK_REALTIME;
-//static const clockid_t clockType = CLOCK_MONOTONIC;
-//static const clockid_t clockType = CLOCK_PROCESS_CPUTIME_ID;
-//static const clockid_t clockType = CLOCK_THREAD_CPUTIME_ID;
 
+//timing variables
 struct timespec start, stop;
-
 unsigned long long diffSum=0ULL;
-
 unsigned long long callTime=0ULL;
-
 unsigned long long diffCount=0;
+//iteration limit (set elsewhere)
+unsigned long long iterCount;
 
-void ping( fifo_t & recv_buf, fifo_t & send_buf)
+int checkCmdLineArgs(int argc, char* argv[], int& fiberCount, unsigned long long& iterationCount);
+void fiberFunction( FifoType& recvBuff, FifoType& sendBuff);
+unsigned long long timeDiff(struct timespec start, struct timespec stop);
+
+int main(int argc, char* argv[])
 {
-    //boost::fibers::fiber::id id(boost::this_fiber::get_id());
-
-    //std::cout << "fiber " <<  id << ": Starting" << std::endl;
+    int fiberCount;
+    if(checkCmdLineArgs(argc, argv, fiberCount, iterCount) != 0)
+    {
+        return 1;
+    }
     
-    for(int i=0; i<iterCount; ++i)
+    // set up the fibers
+    int lastBufferIndex = (fiberCount - 1);
+    // generate the set of buffers
+    std::vector<FifoType*> bufferVector;
+    for(int i=0; i<fiberCount; ++i)
     {
-        send_buf.push("");
+        FifoType* temp = new FifoType(2);
+        bufferVector.emplace_back(temp);
+    }
+
+    // generate the set of fibers
+    std::vector<FiberType> fiberVector;
+    for(int i=0; i<lastBufferIndex; ++i)
+    {
+        fiberVector.emplace_back(FiberType(boost::bind(&fiberFunction,
+                                           boost::ref(*(bufferVector[i])),
+                                           boost::ref(*(bufferVector[i+1])))));
+    }
+
+    //execute the main fiber
+    for(unsigned long long i=0; i<iterCount; ++i)
+    {
+        bufferVector[0]->push("");
         clock_gettime(clockType, &start);
         boost::this_fiber::yield();
 
         std::string value;
+        bufferVector[lastBufferIndex]->pop( value);
         clock_gettime(clockType, &stop);
-        diffSum += (1000000000ULL*(stop.tv_sec - start.tv_sec) + (stop.tv_nsec-start.tv_nsec));
-        recv_buf.pop( value);
+        diffSum += timeDiff(start, stop);
         clock_gettime(clockType, &start);
         clock_gettime(clockType, &stop);
-        callTime += (1000000000ULL*(stop.tv_sec - start.tv_sec) + (stop.tv_nsec-start.tv_nsec));
+        callTime += timeDiff(start, stop);
         ++diffCount;
-        //std::cout << "fiber " <<  id << ": ping received: " << value << std::endl;
         value.clear();
     }
 
-    send_buf.close();
-}
+    bufferVector[0]->close();
 
-void pong( fifo_t & recv_buf, fifo_t & send_buf)
-{
-    //boost::fibers::fiber::id id(boost::this_fiber::get_id());
-
-    for(int i=0; i<iterCount; ++i)
+    for(int i=0; i<lastBufferIndex; ++i)
     {
-        std::string value;
-        clock_gettime(clockType, &stop);
-        diffSum += (1000000000ULL*(stop.tv_sec - start.tv_sec) + (stop.tv_nsec-start.tv_nsec));
-        recv_buf.pop( value);
-        clock_gettime(clockType, &start);
-        clock_gettime(clockType, &stop);
-        callTime += (1000000000ULL*(stop.tv_sec - start.tv_sec) + (stop.tv_nsec-start.tv_nsec));
-        ++diffCount;
-        //std::cout << "fiber " <<  id << ": pong received: " << value << std::endl;
-        value.clear();
-
-        send_buf.push("");
-        clock_gettime(clockType, &start);
-        boost::this_fiber::yield();
+        fiberVector[i].join();
     }
 
-    send_buf.close();
-}
-
-void clong( fifo_t & recv_buf, fifo_t & send_buf)
-{
-    //boost::fibers::fiber::id id(boost::this_fiber::get_id());
-
-    for(int i=0; i<iterCount; ++i)
-    {
-        std::string value;
-        clock_gettime(clockType, &stop);
-        diffSum += (1000000000ULL*(stop.tv_sec - start.tv_sec) + (stop.tv_nsec-start.tv_nsec));
-        recv_buf.pop( value);
-        clock_gettime(clockType, &start);
-        clock_gettime(clockType, &stop);
-        callTime += (1000000000ULL*(stop.tv_sec - start.tv_sec) + (stop.tv_nsec-start.tv_nsec));
-        ++diffCount;
-        //std::cout << "fiber " <<  id << ": clong received: " << value << std::endl;
-        value.clear();
-
-        send_buf.push("");
-        clock_gettime(clockType, &start);
-        boost::this_fiber::yield();
-    }
-
-    send_buf.close();
-}
-
-int main()
-{
-    fifo_t buf1(2), buf2(2), buf3(2), buf4(2);
-
-    boost::fibers::fiber f1(
-            boost::bind(&ping, boost::ref(buf1), boost::ref(buf2)));
-    boost::fibers::fiber f2(
-            boost::bind(&pong, boost::ref(buf2), boost::ref(buf3)));
-    boost::fibers::fiber f3(
-            boost::bind(&clong, boost::ref(buf3), boost::ref(buf4)));
-
-    //boost::fibers::fiber::id id(boost::this_fiber::get_id());
-
-
-    for(int i=0; i<iterCount; ++i)
-    {
-        std::string value;
-        buf4.pop( value);
-        clock_gettime(clockType, &stop);
-        diffSum += (1000000000ULL*(stop.tv_sec - start.tv_sec) + (stop.tv_nsec-start.tv_nsec));
-        clock_gettime(clockType, &start);
-        clock_gettime(clockType, &stop);
-        callTime += (1000000000ULL*(stop.tv_sec - start.tv_sec) + (stop.tv_nsec-start.tv_nsec));
-        ++diffCount;
-        //std::cout << "fiber " <<  id << ": bong received: " << value << std::endl;
-        value.clear();
-
-        buf1.push("");
-        clock_gettime(clockType, &start);
-        boost::this_fiber::yield();
-    }
-
-    buf1.close();
-
-    f1.join();
-    f2.join();
-    f3.join();
-
-    std::cout << "Fiber Context Switch Test Complete." << std::endl;
+    std::cout << "\n\nFiber Context Switch Test Complete." << "  Ran With "
+              << fiberCount << " Fibers and " << iterCount << " Iterations"
+              << std::endl;
 
     std::cout << "    Total Fiber Context Switch Time Difference Was: " << diffSum << "ns across " << diffCount <<
                  " Switches.\n\tAverage Time Difference Was: "
@@ -145,9 +87,89 @@ int main()
                  " Call Pairs.\n\tAverage Call Time Difference Was: "
               << (static_cast<double>(callTime) / static_cast<double>(diffCount))<<" nanoseconds"<<std::endl;
 
-    std::cout << "\n    The average, clock_gettime call compensated, fiber context switch time is: " <<
-                  (static_cast<double>(diffSum-callTime) / static_cast<double>(diffCount))<<" nanoseconds"<<std::endl;
+    std::cout << "\nThe average, clock_gettime call compensated, fiber context switch time is: " <<
+                  (static_cast<double>(diffSum-callTime) / static_cast<double>(diffCount))<<" nanoseconds\n\n"<<std::endl;
 
     return EXIT_SUCCESS;
 }
 
+void fiberFunction(FifoType& recvBuff, FifoType& sendBuff)
+{   
+    for(unsigned long long i=0; i<iterCount; ++i)
+    {
+        std::string value;
+        clock_gettime(clockType, &stop);
+        diffSum += timeDiff(start, stop);
+        recvBuff.pop( value);
+        clock_gettime(clockType, &start);
+        clock_gettime(clockType, &stop);
+        callTime += timeDiff(start, stop);
+        ++diffCount;
+        //std::cout << "fiber " <<  id << ": clong received: " << value << std::endl;
+        value.clear();
+
+        sendBuff.push("");
+        clock_gettime(clockType, &start);
+        boost::this_fiber::yield();
+    }
+
+    sendBuff.close();
+}
+
+unsigned long long timeDiff(struct timespec start, struct timespec stop)
+{
+    return ((1000000000ULL*(stop.tv_sec - start.tv_sec)) + (stop.tv_nsec-start.tv_nsec));
+}
+
+void printHelp(const std::string& call)
+{
+    std::cout<<"Usage:\n    " << call << " [ <Num Fibers To Use> [Num Iterations To Run] ]\n"<<std::endl;
+    std::cout<<"  <Num Fibers To Use> must be in the range [2, 1000] (inclusive) - defaults to 50"<<std::endl;
+    std::cout<<"  [Num Iterations To Run] must be in the range [3, 500000000] (inclusive) - defaults to 1000000"<<std::endl;
+}
+
+int checkCmdLineArgs(int argc, char* argv[], int& fiberCount, unsigned long long& iterationCount)
+{
+    if(argc == 1) // left both to defaults
+    {
+        iterationCount = 1000000;
+        fiberCount = 50;
+    }
+    else if(argc == 2) // specified a fiber count but not an iteration count
+    {
+        iterationCount = 1000000;
+        std::istringstream conv;
+        conv.str(argv[1]);
+        conv >> fiberCount;
+        if((fiberCount <2) || (fiberCount > 100))
+        {
+            printHelp(argv[0]);
+            return 1;
+        }
+    }
+    else if(argc == 3) // specified both a fiber and iteration count
+    {
+        std::istringstream conv;
+        conv.str(argv[1]);
+        conv >> fiberCount;
+        if((fiberCount < 2) || (fiberCount > 100))
+        {
+            printHelp(argv[0]);
+            return 1;
+        }
+        conv.clear();
+        conv.str(argv[2]);
+        conv >> iterationCount;
+        if((iterationCount < 3) || (iterationCount > 500000000))
+        {
+            printHelp(argv[0]);
+            return 1;
+        }
+    }
+    else // specified more cmd line options that we accept
+    {
+        printHelp(argv[0]);
+        return 1;
+    }
+    return 0;
+}
